c4b7c2735be378af74ef5b2aca87b291de0878bc
Lukasz Cena <lbjcena@gmail.com>
2023-12-03 21:01:21 +0100

    first iteration - calculate using objects, finish later

--- js/script.js
+++ js/script.js
@@ -5,14 +5,35 @@ let screen = document.querySelector('input')
 screen.value = ''
 const btnSpecial = document.querySelectorAll('.btn-special')
 
-let pairOfNumbers = (num1, num2, operation) => {
-  switch (operation) {
-    case ('add'):
-      return num1 + num2
-    case ('sub'):
-      return num1 - num2
-    case ('mult'):
-      return parseFloat(num1 / num2).toFixed(2)
+// let pairOfNumbers = (num1, num2, operation) => {
+//   switch (operation) {
+//     case ('add'):
+//       return num1 + num2
+//     case ('sub'):
+//       return num1 - num2
+//     case ('mult'):
+//       return parseFloat(num1 / num2).toFixed(2)
+//   }
+// }
+
+class Calculation {
+  constructor(num1, num2, operation) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.operation = operation
+  }
+
+  calc(){
+    switch(operation) {
+      case 'add':
+        return this.num1 + this.num2
+      case 'sub':
+        return this.num1 - this.num2
+      case 'mult':
+        return this.num1 * this.num2
+      case 'div':
+        return parseFloat(this.num1 / this.num2).toFixed(2)
+    }
   }
 }
 
@@ -21,3 +42,9 @@ btnNum.forEach((button) => {
     screen.value += button.textContent
   })
 })
+const run = new Calculation()
+
+const runApp = () => {
+
+}
+
