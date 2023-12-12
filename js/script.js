f207f9bf72d376c43433990a2374d29062360eb4
LBJ <lbjcena@gmail.com>
2023-12-12 14:18:11 +0100

    reworked

--- js/script.js
+++ js/script.js
@@ -3,80 +3,49 @@
 const btn = document.querySelectorAll('.btn')
 const screen = document.querySelector('input')
 screen.value = ''
+const add = (a, b) => {
+  return a + b
+}
 
-const calculationsStorage = []
-let currentInput = ''
+const subtract = (a, b) => {
+  return a - b
+}
 
-class Calculation {
-  constructor (num1, num2, operation) {
-    this.num1 = num1
-    this.num2 = num2
-    this.operation = operation
-  }
+const multiply = (a, b) => {
+  return a * b
+}
 
-  getParam (param) {
-    return param
-  }
-
-  setParam (param, newValue) {
-    this.param = newValue
-  }
-
-  calc () {
-    switch (this.operation) {
-      case '+':
-        return parseFloat(this.num1 + this.num2).toFixed(2)
-      case '-':
-        return parseFloat(this.num1 - this.num2).toFixed(2)
-      case '*':
-        return parseFloat(this.num1 * this.num2).toFixed(2)
-      case '/':
-        return parseFloat(this.num1 / this.num2).toFixed(2)
-    }
-  }
-
-  store (container) {
-    container.push([this.num1, this.num2, this.operation])
+const divide = (a, b) => {
+  return parseFloat(a / b).toFixed(2)
+}
+const calculate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      add(a, b)
+      break
+    case '-':
+      subtract(a, b)
   }
 }
 
 const runApp = () => {
+  let firstNum = 0
+  let secondNum = 0
+  // let operator = null
   btn.forEach((button) => {
     button.addEventListener('click', () => {
       if (button.classList.contains('btn-number')) {
-        currentInput += button.textContent
-        screen.value = currentInput
-      } else if (button.classList.contains('btn-special')) {
-        currentInput += ` ${button.textContent} `
-        screen.value = currentInput
+        if (screen.value === '') {
+          firstNum = parseInt(button.innerText)
+          screen.value += button.innerText
+        } else {
+          secondNum = parseInt(button.innerText)
+          screen.value += button.innerText
+        }
       }
+      console.log(`FIRST : ${firstNum} | SECOND: ${secondNum}`)
     })
-  })
-
-  document.querySelector('#result').addEventListener('click', () => {
-    if (currentInput.trim() !== '') {
-      const parts = currentInput.split(' ')
-      const num1 = parseFloat(parts[0])
-      const num2 = parseFloat(parts[2])
-      const operation = parts[1]
-
-      if (!isNaN(num1) && !isNaN(num2) && operation) {
-        const newCalculation = new Calculation(num1, num2, operation)
-        const finalResult = newCalculation.calc()
-        screen.value = finalResult
-        currentInput = finalResult
-      } else {
-        alert('Invalid input')
-      }
-    }
   })
 }
 
 runApp()
-
-/**
- * add a check in the forEach loop
- * if button's class has 'btn-number' - assign it to num1 or num2, if num1 exists
- * if it has 'btn-special', assign the operator
- * if its result, return the result
- */
